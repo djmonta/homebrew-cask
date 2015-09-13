8ceaa70188818e82b916092f65f4291c6f0bd870
monta <djmonta@gmail.com>
2015-09-13 20:52:21 +0900

    Add iterm2-margin

---
+++ Casks/iterm2-margin.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'iterm2-margin' do
+  # note: "2" is not a version number, but indicates a different vendor
+  version '2.1.1-margin'
+  sha256 '052d6f9fd516a1c799349854b0ad0d63646142acef4c01cae014b66dd659fff0'
+
+  url 'https://github.com/djmonta/iTerm2/releases/download/v2.1.1-margin/iTerm2.app.zip'
+
+  name 'iTerm2'
+  homepage 'https://www.iterm2.com/'
+  license :gpl
+
+  app 'iTerm.app'
+
+  zap :delete => '~/Library/Preferences/com.googlecode.iterm2.plist'
+
+end
