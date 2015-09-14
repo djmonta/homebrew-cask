317937d4b11953b8b40d4910ccf77cfbad654466
monta <djmonta@gmail.com>
2015-09-14 11:00:20 +0900

    sha256 change

--- Casks/iterm2-margin.rb
+++ Casks/iterm2-margin.rb
@@ -1,7 +1,7 @@
 cask :v1 => 'iterm2-margin' do
   # note: "2" is not a version number, but indicates a different vendor
   version '2.1.1-margin'
-  sha256 '052d6f9fd516a1c799349854b0ad0d63646142acef4c01cae014b66dd659fff0'
+  sha256 'bcbe67b771c77d4fdee98161f0fb23c4869252cc4ab35273b65aa414a6c21749'
 
   url 'https://github.com/djmonta/iTerm2/releases/download/v2.1.1-margin/iTerm2.app.zip'
 
